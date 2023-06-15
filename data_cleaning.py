a098e913a14dfe7adeefff2adc6608e6ae91bc15
Sadra <sadra@MacBook-Pro.local>
2023-06-15 13:36:25 -0700

    data is fixed

--- data_cleaning.py
+++ data_cleaning.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 
-df = pd.read_csv("./data/census.csv")
+df = pd.read_csv("./data/census.csv",delimiter=',', encoding="utf-8-sig")
 
 categorical_features_indices = np.where(df.dtypes == object)[0]
 cat_features = df.columns[categorical_features_indices].tolist()
@@ -9,7 +9,7 @@ cat_features = df.columns[categorical_features_indices].tolist()
 
 df.columns = [c.strip() for c in df.columns]
 
-for c in cat_features:
-    df[c] = df[c].str.strip()
+#for c in cat_features:
+#    df[c] = df[c].str.strip()
 
 df.to_csv("./data/census.csv", index=False)
