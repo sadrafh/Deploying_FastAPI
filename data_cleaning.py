1017b947f3ad2ec8c74ce21339c44e3d9cc33d70
Sadra <sadra@MacBook-Pro.local>
2023-06-15 11:17:42 -0700

    2n commit

---
+++ data_cleaning.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import numpy as np
+
+df = pd.read_csv("./data/census.csv")
+
+categorical_features_indices = np.where(df.dtypes == object)[0]
+cat_features = df.columns[categorical_features_indices].tolist()
+
+
+df.columns = [c.strip() for c in df.columns]
+
+for c in cat_features:
+    df[c] = df[c].str.strip()
+
+df.to_csv("./data/census.csv", index=False)
--- model/train_model.py
+++ model/train_model.py
@@ -39,10 +39,66 @@ cat_features = [
     "sex",
     "native-country",
 ]
+
+
 X_train, y_train, encoder, lb = process_data(
-    train, categorical_features=cat_features, label="salary", training=True
+    train,
+    categorical_features=cat_features,
+    label="salary",
+    training=True
 )
 
 # Proces the test data with the process_data function.
+# Set train flag = False - We use the encoding from the train set
+X_test, y_test, encoder, lb = process_data(
+    test,
+    categorical_features=cat_features,
+    label="salary",
+    training=False,
+    encoder=encoder,
+    lb=lb
+)
 
-# Train and save a model.
+# check if trained model already exists
+savepath = '../model'
+filename = ['trained_model.pkl', 'encoder.pkl', 'labelizer.pkl']
+
+# if saved model exits, load the model from disk
+if os.path.isfile(os.path.join(savepath,filename[0])):
+        model = pickle.load(open(os.path.join(savepath,filename[0]), 'rb'))
+        encoder = pickle.load(open(os.path.join(savepath,filename[1]), 'rb'))
+        lb = pickle.load(open(os.path.join(savepath,filename[2]), 'rb'))
+
+# Else Train and save a model.
+else:
+    model = train_model(X_train, y_train)
+    # save model  to disk in ./model folder
+    pickle.dump(model, open(os.path.join(savepath,filename[0]), 'wb'))
+    pickle.dump(encoder, open(os.path.join(savepath,filename[1]), 'wb'))
+    pickle.dump(lb, open(os.path.join(savepath,filename[2]), 'wb'))
+    logging.info(f"Model saved to disk: {savepath}")
+
+
+# evaluate trained model on test set
+preds = inference(model, X_test)
+precision, recall, fbeta = compute_model_metrics(y_test, preds)
+
+logging.info(f"Classification target labels: {list(lb.classes_)}")
+logging.info(
+    f"precision:{precision:.3f}, recall:{recall:.3f}, fbeta:{fbeta:.3f}")
+
+cm = compute_confusion_matrix(y_test, preds, labels=list(lb.classes_))
+
+logging.info(f"Confusion matrix:\n{cm}")
+
+# Compute performance on slices for categorical features
+# save results in a new txt file
+slice_savepath = "./slice_output.txt"
+remove_if_exists(slice_savepath)
+
+# iterate through the categorical features and save results to log and txt file
+for feature in cat_features:
+    performance_df = compute_slices(test, feature, y_test, preds)
+    performance_df.to_csv(slice_savepath,  mode='a', index=False)
+    logging.info(f"Performance on slice {feature}")
+    logging.info(performance_df)
